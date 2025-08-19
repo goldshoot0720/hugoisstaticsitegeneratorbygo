191a8646c25260fc9018ace5d62751c3bd79f4a5
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-20 00:56:09 +0800

    20250820 v1

---
+++ clean.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\chbon\Documents\hugoisstaticsitegeneratorbygo"
+hugo --cleanDestinationDir
+pause
---
+++ clhs9331143.github.io.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\chbon\Documents\hugoisstaticsitegeneratorbygo"
+git init
+git add .
+git commit -m "20250819 v3"
+git remote add origin https://github.com/clhs9331143/clhs9331143.github.io.git
+git checkout -b main
+git push -u origin main --force
+pause
